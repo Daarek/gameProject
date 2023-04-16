f367001785422db495355f347bf98375e4ae166a
Daarek <ggosin090@gmail.com>
2023-04-16 20:05:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Character.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+public class Character { //мэин хиро
+    public int x; // x y на поле
+    public int y;
+    public int layoutX; // x y на экране
+    public int layoutY;
+    public Character (int x, int y){
+        this.x = x;
+        this.y = y;
+        this.layoutX = x * 10;
+        this.layoutY = y * 10;
+    }
+
+    public void setX(int x){ //чтобы всегда знал где находится
+        this.x = x;
+        layoutX = x * 10;
+    }
+    public void setY(int y){
+        this.y = y;
+        layoutY = y * 10;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout layout = findViewById(R.id.layout); // ищу экран (а где?)
+    TextView map[][] = new TextView[20][20]; //массив текствьювов (когда узнал что оно работает чуть от радости не помер)
+    public int mapStatus[][] = new int[20][20];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int x = 0; x < 20; x++) { //генерирую карту
+            for (int y = 0; y < 20; y++){  //oh no
+                mapStatus[x][y] = 0;
+                map[x][y] = new TextView(this);
+                map[x][y].setText(mapStatus[x][y]); //оно же сработает?
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT); //oh no x2
+                params.setMargins(x * 10, y * 10, 0, 0); //пофиг на новую переменную, локальная же
+                map[x][y].setLayoutParams(params); //делаем отступы и забываем про локал до следующей итерации
+                map[x][y].setHeight(10);
+                map[x][y].setWidth(10);
+                layout.addView(map[x][y]);//чуть не забыл отобразить (да как это бл еще не сломалось?)
+            }
+        }
+
+        Character me = new Character(5, 6);//создаю персонажа
+        movement(0, 5, 0, 6); //замена нулевых координат на 0 никому не повредит, всё равно не могу оставить пустым/null
+
+
+
+    }
+
+    public void movement (int prevX, int X, int prevY, int Y) {
+        mapStatus[prevX][prevY] = 0;
+        mapStatus[X][Y] = 1;
+        map[prevX][prevY].setText(mapStatus[prevX][prevY]);
+        map[X][Y].setText(mapStatus[X][Y]);
+    }
+
+}
