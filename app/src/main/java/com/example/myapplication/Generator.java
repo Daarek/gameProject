f36d9c535fbf105a1fabd5e297127e63e04413af
Daarek <110654512+Daarek@users.noreply.github.com>
2023-04-17 09:11:03 +0700

    Blueprint for generator class

---
+++ app/src/main/java/com/example/myapplication/Generator.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+public class Generator {
+
+  public int generate () {
+    int generated = Math.random();
+    if (generated < 0.6){
+      return 0; //empty tile
+    } else if (generated < 0.7){
+      return 1; //tree
+    } else if (generated < 0.8){
+      return 2; //stone
+    } else if (generated < 0,9){
+      return 3; //grass
+    } else if (generated < 1) {
+      return 4;//bush
+    } else {
+      //обработать ошибку генератора
+    }
+  }
+
+}
