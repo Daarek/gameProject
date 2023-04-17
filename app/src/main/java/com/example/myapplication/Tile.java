e5ea8195500f90024f064ac3d7863c9c201772a8
Daarek <110654512+Daarek@users.noreply.github.com>
2023-04-17 09:00:31 +0700

    Blueprint for tile class

---
+++ app/src/main/java/com/example/myapplication/Tile.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+public class Tile {
+    public class tree {
+      public int resource;
+      public int size;
+      public int type; 
+    }
+    public class stone {
+      public int resource;
+      public int size;
+      public int type;
+    }
+    public class grass {
+      public int size;
+      public int resource;
+    }
+    public class bush {
+      public int resource;
+      public int size;
+      public int type;
+    }
+}
