ba830b6a5c8123aa2a1caf7a95d48e382cd8d56a
Daarek <110654512+Daarek@users.noreply.github.com>
2023-05-05 14:41:14 +0700

    Preparation for class rework

--- app/src/main/java/com/example/myapplication/Character.java
+++ app/src/main/java/com/example/myapplication/Character.java
@@ -1,23 +1,10 @@
 package com.example.myapplication;
 
 public class Character { //мэин хиро
-    public int x; // x y на поле
-    public int y; //!!!переделать координаты под биткарту!!!
-    public int layoutX; // x y на экране
-    public int layoutY;
+    public int x; // x и y
+    public int y; 
     public Character (int x, int y) {
-        this.x = x;
-        this.y = y;
-        this.layoutX = x * 10;
-        this.layoutY = y * 10;
     }
 
-    public void setX(int x){ //чтобы всегда знал где находится
-        this.x = x;
-        layoutX = x * 10;
-    }
-    public void setY(int y){
-        this.y = y;
-        layoutY = y * 10;
-    }
+   
 }
