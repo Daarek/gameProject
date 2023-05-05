27fc1053e155cf76c836b1f3c3e8543e765f8084
Daarek <110654512+Daarek@users.noreply.github.com>
2023-05-05 17:41:16 +0700

    Update Character.java

--- app/src/main/java/com/example/myapplication/Character.java
+++ app/src/main/java/com/example/myapplication/Character.java
@@ -1,6 +1,6 @@
 package com.example.myapplication;
 
-public class Character { //мэин хиро
+public class Character extends View { //мэин хиро
     public int x; // x и y
     public int y; 
     public Character (int x, int y) {
