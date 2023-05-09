ad105e7998b20145f8d80353c8828417fea0964d
Daarek <110654512+Daarek@users.noreply.github.com>
2023-05-09 13:04:52 +0700

    comments for Character.java

--- app/src/main/java/com/example/myapplication/Character.java
+++ app/src/main/java/com/example/myapplication/Character.java
@@ -11,30 +11,30 @@ import android.view.View;
 public class Character extends View { //мэин хиро
     public int x; // x и y
     public int y;
-    public MapData mapData;
-    public double shift;
-    public int centerX;
+    public MapData mapData; //данные о карте
+    public double shift; //во сколько раз  ширина экрана меньше высоты
+    public int centerX; //координаты центра экрана
     public int centerY;
     public Character (Context context, int X, int Y) {
         super(context);
-        x = X;
+        x = X; //получаю стартовые координаты игрока
         y = Y;
         Resources res = getResources();
         DisplayMetrics metrics = res.getDisplayMetrics();
-        int width = metrics.widthPixels;
+        int width = metrics.widthPixels; //получаю размеры экрана
         int height = metrics.heightPixels;
-        centerX = width/2;
+        centerX = width/2; //получаю координаты центра
         centerY = height/2;
-        shift = (double) width/height;
+        shift = (double) width/height; //получаю шифт (ширина/высота)
 
     }
     public void setReferences(){
         Setup setup = MainActivity.getSetup();
-        mapData = setup.getMapData();
+        mapData = setup.getMapData(); //получаю мапдату
     }
     public void create(){
-        mapData.type[x][y] = PLAYER;
-        mapData.playerPos[0] = x;
+        mapData.type[x][y] = PLAYER; //координаты игрока на битмапе (поверх тайлов)
+        mapData.playerPos[0] = x; //координаты игрока в .type
         mapData.playerPos[1] = y;
     }
     public void move(double actualX, double eventY){
