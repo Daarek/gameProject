1dc15b6f14c034163e1d54b4a1b044119ed17410
Daarek <ggosin090@gmail.com>
2023-07-26 14:48:18 +0700

    resources and interactions branch commit

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -2,10 +2,14 @@ package com.example.myapplication;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.zip.Inflater;
 
 public class Menu {
     AlertDialog menu;
@@ -14,22 +18,45 @@ public class Menu {
     AlertDialog.Builder interactionBuilder;
     Button interact;
     TextView amount;
+    TextView type;
+    MapData mapData;
+    Setup setup;
 
     public Menu () {
 
     }
 
     public void setup () {
+        setup = MainActivity.getSetup();
+        mapData = setup.getMapData();
+
         menuBuilder = new AlertDialog.Builder(MainActivity.Context());
-        menuBuilder.setView(R.layout.menu);
-        menu = menuBuilder.create();
         interactionBuilder = new AlertDialog.Builder(MainActivity.Context());
-        interactionBuilder.setView(R.layout.interaction);
+        LayoutInflater menuInflater = LayoutInflater.from(MainActivity.Context());
+        View menuView = menuInflater.inflate(R.layout.menu, null);
+        LayoutInflater interactionInflater = LayoutInflater.from(MainActivity.Context());
+        View interactionView = interactionInflater.inflate(R.layout.interaction, null);
+        menuBuilder.setView(menuView);
+        interactionBuilder.setView(interactionView);
+        menu = menuBuilder.create();
         interaction = interactionBuilder.create();
-        //доделать тут
+        interact = menuView.findViewById(R.id.Interact);
+        amount = interactionView.findViewById(R.id.amountView);
+        type = interactionView.findViewById(R.id.typeView);
+        interact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                amount.setText(Integer.toString(mapData.amount[mapData.playerPos[0]][mapData.playerPos[1]]));
+                type.setText(mapData.floor[mapData.playerPos[0]][mapData.playerPos[1]].toString());
+                interaction.show();
+            }
+        });
+
     }
+
 
     public void alert () {
         menu.show();
     }
 }
+
--- app/src/main/java/com/example/myapplication/Tile.java
+++ app/src/main/java/com/example/myapplication/Tile.java
@@ -2,11 +2,18 @@ package com.example.myapplication;
 
 public enum Tile { //типы тайлов
 
-    EMPTY,
-    TREE,
-    STONE,
-    BUSH,
-    GRASS,
-    PLAYER,
+    EMPTY("Empty"),
+    TREE("Tree"),
+    STONE("Stone"),
+    BUSH("Bush"),
+    GRASS("Grass"),
+    PLAYER("Player");
 
+    private String display;
+    Tile (String display){ //Я фиг его знает как это работает
+        this.display = display;
+    }
+    public String toString(){
+        return display;
+    }
 }
